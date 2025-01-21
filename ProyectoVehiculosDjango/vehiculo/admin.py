32651132132f50178eb908999742f475fc5bd10b
merojasr <mrojas2103@gmail.com>
2025-01-21 12:46:24 -0300

    Delete ProyectoVehiculosDjango directory

--- ProyectoVehiculosDjango/vehiculo/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from . models import Vehiculo
-
-# Register your models here.
-
-admin.site.register(Vehiculo)
--- ProyectoVehiculosDjango/vehiculo/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from vehiculo.models import Vehiculo
-
-class VehiculoForm(ModelForm):
-    class Meta:
-        model = Vehiculo
-        fields = ['marca', 'modelo', 'serialCarroceria', 'serialMotor', 'categoria', 'precio']
